9e82221185763b565abdc3140923d2fe0d62dc79
Thierry Charbonnel <thierry@creationflux.com>
2013-11-05 11:15:52 +0100

    update lineBreak rules…

--- src/commands/insertLineBreak.js
+++ src/commands/insertLineBreak.js
@@ -1,20 +1,22 @@
 (function(wysihtml5) {
   var LINE_BREAK = "<br>" + (wysihtml5.browser.needsSpaceAfterLineBreak() ? " " : "");
-  
+    
   wysihtml5.commands.insertLineBreak = {
     exec: function(composer, command) {
-      if (composer.commands.support(command)) {
-        composer.doc.execCommand(command, false, null);
-        if (!wysihtml5.browser.autoScrollsToCaret()) {
-          composer.selection.scrollIntoView();
-        }
-      } else {
-        composer.commands.exec("insertHTML", LINE_BREAK);
-      }
+			if (!composer.selection.getSelection().isCollapsed) return;
+		
+			if (composer.commands.support(command)) {
+				composer.doc.execCommand(command, false, null);
+				if (!wysihtml5.browser.autoScrollsToCaret()) {
+					composer.selection.scrollIntoView();
+				}
+			} else {
+				composer.commands.exec("insertHTML", LINE_BREAK);
+			}
     },
 
-    state: function() {
-      return false;
+    state: function(composer, command) {
+      return composer.selection.getSelection().isCollapsed;
     }
   };
 })(wysihtml5);
